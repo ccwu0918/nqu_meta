7047ec77337c5a89b468bfbb45a25041fe95af51
Chia-Chun Wu <ccwu0918@gmail.com>
2025-11-05 18:41:13 +0800

    Reduce mint price from 0.001 to 0.0001 ether

--- contracts/nqu_meta_nft.sol
+++ contracts/nqu_meta_nft.sol
@@ -14,7 +14,7 @@ contract NquMeta is ERC721Enumerable, Ownable {
 
     // Constants
     uint256 public constant MAX_SUPPLY = 10;
-    uint256 public mintPrice = 0.001 ether;
+    uint256 public mintPrice = 0.0001 ether;
     uint256 public maxBalance = 1;
     uint256 public maxMint = 1;
 
